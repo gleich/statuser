fc014e7a8c6ad0cc736cd6884c4b7bfb891b9159
Matthew Gleich <matthewgleich@gmail.com>
2020-06-15 21:50:11 -0400

    ✨ Add Error function
    Signed-off-by: Matthew Gleich <matthewgleich@gmail.com>

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/Matt-Gleich/statuser
 
 go 1.14
+
+require (
+	github.com/fatih/color v1.9.0
+	github.com/mattn/go-colorable v0.1.6 // indirect
+	golang.org/x/sys v0.0.0-20200615200032-f1bc736245b1 // indirect
+)
--- main.go
+++ main.go
@@ -1,5 +1,32 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"unicode/utf8"
 
+	"github.com/fatih/color"
+)
+
+var emojis = true
+
+var blockChar = "░"
+
+func generateBlock(message, surroundingChar string) string {
+	messageLen := utf8.RuneCountInString(message)
+	var topAndBottom string
+	for i := 0; i < messageLen+4; i++ {
+		topAndBottom = topAndBottom + surroundingChar
+	}
+	return fmt.Sprintf("%v\n%v%v%v\n%v", topAndBottom, surroundingChar, message, surroundingChar, topAndBottom)
+}
+
+// Error ... Output an error to the user
+func Error(message string, err error) {
+	title := "ERROR"
+	if emojis {
+		title = "🚨 ERROR 🚨"
+	}
+	color.Red(generateBlock(title, "░"))
+	color.Red("\n" + message)
+	color.Red("\nGOLANG ERROR (SHOW DEVELOPER):\n" + err.Error())
 }
